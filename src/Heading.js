bdbd50ed0061362124955eff49782340e48d7b63
Jarquevious <jarquevious@gmail.com>
2021-03-03 17:24:36 -0800

    first commit

---
+++ src/Heading.js
@@ -0,0 +1,13 @@
+// src/Heading.js
+import React from 'react'
+
+function Heading() {
+    return (
+      <div>
+        <h1>FEW 1.2 Final Assessment </h1>
+        <h2>Jarquevious Nelson</h2>
+      </div>
+    )
+  }
+  
+  export default Heading
